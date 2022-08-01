74aa9df31062d874991faa145930a8817dc249c8
Jesus David Muñoz Martinez <david.munoz@sonatafy.com>
2022-08-01 13:59:18 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module example/hello
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/slack-go/slack v0.11.2 // indirect
+)
---
+++ slack-daily-meeting-order.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/socketmode"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	token := os.Getenv("SLACK_AUTH_TOKEN")
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	api := slack.New(
+		token,
+		slack.OptionDebug(true),
+		slack.OptionAppLevelToken(appToken),
+	)
+	socketClient := socketmode.New(
+		api,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	go func() {
+		for evt := range socketClient.Events {
+			switch evt.Type {
+			case socketmode.EventTypeSlashCommand:
+				cmd, ok := evt.Data.(slack.SlashCommand)
+				if !ok {
+					fmt.Printf("Ignored %+v\n", evt)
+					continue
+				}
+
+				blocks := []slack.Block{
+					slack.NewSectionBlock(&slack.TextBlockObject{
+						Type: slack.MarkdownType,
+						Text: "Daily standup: ",
+					}, nil, nil),
+				}
+
+				switch cmd.Command {
+				case "/meeting-order":
+					users, _, err := socketClient.GetUsersInConversation(&slack.GetUsersInConversationParameters{ChannelID: cmd.ChannelID})
+					if err != nil {
+						fmt.Println("Something went wrong", err)
+					}
+					Shuffle(users)
+					count := 0
+					for _, user := range users {
+						fmt.Println(user)
+						info, err := socketClient.GetUserInfo(user)
+						if err != nil {
+							fmt.Println("Something went wrong", err)
+						}
+						if !info.IsBot {
+							count++
+							blocks = append(
+								blocks,
+								MakeSimpleTextSectionBlock(strconv.FormatInt(int64(count), 10)+" - <@"+user+">"))
+						}
+					}
+				default:
+					blocks = append(blocks, MakeSimpleTextSectionBlock("Unknown command :("))
+				}
+
+				payload := map[string]interface{}{
+					"blocks":        blocks,
+					"response_type": "in_channel",
+				}
+
+				socketClient.Ack(*evt.Request, payload)
+			default:
+				fmt.Fprintf(os.Stderr, "Unexpected event type received: %s\n", evt.Type)
+			}
+		}
+	}()
+
+	socketClient.Run()
+}
+
+func Shuffle(a []string) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+}
+
+func MakeSimpleTextSectionBlock(text string) slack.Block {
+	block := slack.NewSectionBlock(
+		&slack.TextBlockObject{
+			Type: slack.MarkdownType,
+			Text: text,
+		},
+		nil,
+		nil,
+	)
+	return block
+}
